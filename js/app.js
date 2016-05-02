05d7938288010943bc1c5f9d564c895adeec923a
jberthiaume1138 <joenyc23@gmail.com>
2016-05-02 02:55:42 -0400

    static page creation start

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 'use strict'
+
+$(document).ready(function(){
+
+});
+
+var getDate = function () {
+
+};
+
+// ----
+// store data about each comic book series in an object Series
+// all of the users subscriptions are stored in array myCollection
+// detail pulled from Marvel API JSON
+var myCollection = [{
+	id:'0',
+	title:'',
+	ongoing:''
+}
+
+];
