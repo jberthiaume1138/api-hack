502d056fc5d48d82767e9f5a7b0da4194d0954cf
jberthiaume1138 <joenyc23@gmail.com>
2016-05-09 01:50:52 -0400

    wikipedia API functional

--- js/app.js
+++ js/app.js
@@ -5,7 +5,8 @@ $(document).ready(function(){
 });
 
 (function() { 
-	'use strict'
+
+'use strict'
 
 var FishFinder = { 
 
@@ -21,7 +22,7 @@ var FishFinder = {
 	    		$.each(data.items, function(i,item) {
 	    		console.log(item);
 
-	     		$("<img />").attr("src", item.media.m).appendTo("#images");
+	     		FishFinder.generateOutput(item);
 
 
 	     		if ( i == 4 ) return false;
@@ -33,15 +34,39 @@ var FishFinder = {
 
 	getVideos: function(tag) {
 
-	}
+	},
 
+	getInfo: function(tag) {
+		var url = "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=" + tag + "&callback=?";
+		$.ajax({
+	        type: "GET",
+	        url: url,
+	        contentType: "application/json; charset=utf-8",
+	        async: false,
+	        dataType: "json",
+	        success: function (data, textStatus, jqXHR) {
+	            console.log(data);
+	        },
+	        error: function (errorMessage) {
+	        }
+   		 });
+
+
+
+	},
+
+	generateOutput: function(item) {
+		$("<img />").attr("src", item.media.m).appendTo("#images");
+	}
 
 
 }; // end of object
 
 $('#btnSearch').click(function() {
 	$('#images').empty();
-	FishFinder.getPictures($('#inputFinder').val());
+	var tag = $('#inputFinder').val();
+	FishFinder.getPictures(tag);
+	FishFinder.getInfo(tag);
 	$('#inputFinder').val('');
 });
 
