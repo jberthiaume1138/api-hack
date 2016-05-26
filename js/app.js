6d6c7f60671f48484229950bd1179a87dd5e0bcb
jberthiaume1138 <joenyc23@gmail.com>
2016-05-26 03:30:19 -0400

    beginning randomizer

--- js/app.js
+++ js/app.js
@@ -50,7 +50,7 @@ var FishFinder = {
 		$.when(promiseImages,promiseVideos,promiseWiki).done(function(imageData,videoData,wikiData) {	
 			// debugging to make sure the AJAX calls are functioning
 			// console.log(imageData);
-			console.log(videoData);
+			// console.log(videoData);
 			// console.log(wikiData);
 
 			FishFinder.generateWiki(wikiData,tag);
@@ -62,11 +62,12 @@ var FishFinder = {
 	generateMasonryOutput: function(imageData,videoData) {
 		// the APIs that populate imageData and videoData return vastly different JSON payloads
 		// so define a class to store the relevant info from either to make it consistent
-		function Gallery (source,thumbnail_url,fullsize_url,title) {
+		function Gallery (source,thumbnail_url,fullsize_url,title,iframe) {
 			this.source = source;
 			this.thumbnail_url = thumbnail_url;
 			this.fullsize_url = fullsize_url;
 			this.title = title;
+			this.iframe = iframe;
 		};
 
 		// array to store each Gallery object
@@ -97,8 +98,11 @@ var FishFinder = {
 
 			objGallery.source = 'youtube';
 			objGallery.thumbnail_url = item.snippet.thumbnails.medium.url;
-			objGallery.fullsize_url = '<iframe width="420" height="315" src="https://www.youtube.com/watch?v=' + item.id.videoId + '" </iframe>';
+			objGallery.fullsize_url = '';
 			objGallery.title = item.snippet.title;
+			objGallery.iframe = '<iframe width="420" height="315" src="https://www.youtube.com/watch?v=' + item.id.videoId + '"></iframe>';
+
+			// console.log(objGallery.iframe);
 
 			collection.push(objGallery);	//store each object in collection array
 		});
@@ -110,90 +114,69 @@ var FishFinder = {
 			var html = '';
 			html += '<div class="grid-item">';
 			html += '<img src="' + collection[i].thumbnail_url + '" class="gallery-image" alt="' + collection[i].title;
-			html += '" data-image="' + collection[i].fullsize_url;
+			// html += '" data-image="' + collection[i].fullsize_url;
+			// html += '" data-youtube="' + collection[i].iframe;
 			html += '">';
 			html += '</div>';
 			$('#gallery').append(html);
 		};
 
-		//create html v2.0 - randomize
+		
+
+		for (var i = 0; i < collection.length; i++) {
+			var mine = collection[Math.floor(Math.random()*collection.length)];
+		 	console.log(mine);
+		 	collection.pop[mine];
+		 	console.log(collection.length);
+		};
+
+		// //create html v2.0 - randomize
 		// while (collection.length > 0) {
-		// 	// generate random value in the range of collection index
-		// 	// pop an item
-		// 	// process it
+		// // 	// generate random value in the range of collection index
+		// // 	// pop an item
+		// // 	// process it
+		// 	var mine = collection[Math.floor(Math.random()*collection.length)];
+		// 	console.log(mine);
+		// 	collection.pop[mine];
+
 		// };
 
 
 		// activate Masonry
-		// need to make sure this doesn't happen till after the images load in the DIV
-		// perhaps use a time out...imagesLoad
-		// var elem = document.querySelector('.grid');
-		// elem.imagesLoaded( function() {
-		// 	var msnry = new Masonry( elem, {
-	 //  			// options
-	 // 			itemSelector: '.grid-item',
-	 // 			gutter: 10,
-	 //  			columnWidth: 240
-		// 	});
-		// });
-
+		// using imagesLoaded to make sure this doesn't happen till after the images load in the DIV
 		var elem = document.querySelector('.grid');
 		imagesLoaded( elem, function() {
-
-			// console.log("imagesLoaded");
-
 			var msnry = new Masonry( elem, {
 				itemSelector: '.grid-item',
 	 			gutter: 10,
-	  			columnWidth: 240
+	  			columnWidth: 320
 			});
 		});
 
 
-		// $('.grid').imagesLoaded( function() {
-		//   // images have loaded
-		// });
-
-		// // options
-		// $('.grid').imagesLoaded( {
-		//   // options...
-		//   },
-		//   function() {
-		//     // images have loaded
-		//   }
-		// );
-
-
-
-		// var $grid = $('.grid').imagesLoaded( function() {
-	 //  		// init Masonry after all images have loaded
-	 //  		$grid.masonry({
-	 //    		// options...
-	 //    		itemSelector: '.grid-item',
-	 // 			gutter: 10,
-	 //  			columnWidth: 240
-	 //  		});
-		// });
 
 
 		// // event handlers for the elements created after the AJAX calls
 		// $('.gallery-image').on('click', function() {
 		// 	// handler to activate modal overlay 
-
+		// 	console.log('clicked image');
 		// 	$('.gallery-modal').css('display', 'block');
 
 		// 	console.log(this);
 
-		// 	var bigImageHTML = '';
-		// 	bigImageHTML += '<img src="images/x-close.gif" class="close">';
+		// 	// var bigImageHTML = '';
+		// 	// bigImageHTML += '<img src="images/x-close.gif" class="close">';
 
-		// 	var url = $(this).data("image");
+		// 	var iframeHTML = $(this).data("youtube");
+		// 	$('.gallery-modal').append(iframeHTML);
+			
+		// 	console.log(iframeHTML);
 
-		// 	bigImageHTML += '<img src="' + url  + '">';
+		// 	// bigImageHTML += '<img src="' + url  + '">';
 
-		// 	console.log(bigImageHTML);
+		// 	// console.log(bigImageHTML);
 
-		// 	$('.gallery-modal').append(bigImageHTML);
+		// 	// $('.gallery-modal').append(bigImageHTML);
 		// });
 
 		// $('.close').on('click', function() {
